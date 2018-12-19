8e39597094b21bd8c1ed47b8159525989e844658
c00cjz00 <summerhill001@gmail.com>
2018-12-19 08:28:03 +0800

    init

---
+++ slide.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+
+    <title>Slide</title>
+
+    <link rel='stylesheet' 
+          type='text/css'
+          media='screen'
+          href='https://openseadragon.github.io/css/style.css'/>
+
+    <script src="https://openseadragon.github.io/openseadragon/openseadragon.min.js"></script>
+</head>
+
+<body>
+
+
+
+<div id="container">
+    <div id="example-wide-dzi" 
+         class="openseadragon">
+        <script type="text/javascript">
+            OpenSeadragon({
+                id:            "example-wide-dzi",
+                prefixUrl:     "https://openseadragon.github.io/openseadragon/images/",
+                tileSources:   "http://140.110.17.13/microdraw6/2396/2396.dzi"
+            });
+        </script>
+    </div>
+ </div>
+
+
+</body>
+</html>
+
